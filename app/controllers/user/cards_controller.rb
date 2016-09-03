606b93bd1f3171812177b2253453d2d89973ef83
Hamit YAY <hamityay@hotmail.com>
2016-09-03 23:17:33 +0300

    Fixed adding card no from user.

--- app/controllers/user/cards_controller.rb
+++ app/controllers/user/cards_controller.rb
@@ -1,25 +1,22 @@
 class User::CardsController < User::UserApplicationController
 
-  before_action :set_card, only: [:create]
   def new
     @card = Card.new
   end
 
   def create
-    @card.update(params[:card_no])
-    @card.id = current_user.id
-    if @card.save!
-      redirect_to root_path
+    if Card.ids.include? (params[:card][:card_no])
+      @card = Card.find(params[:card][:card_no])
+      @card.user_id = current_user.id
+      @card.card_no = params[:card][:card_no]
+      if @card.save!
+        redirect_to root_path
+      end
+    else
+      redirect_to new_user_card_path
     end
   end
 
   private
-    def set_card
-      if Card.ids.include? (params[:card [:card_no]]).to_i
-        @card = Card.find(params[:card_no])
-      else
-        redirect_to new_user_card_path
-      end
 
-    end
 end
